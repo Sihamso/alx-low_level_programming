a6f4eb47b5d2bddee08165d3c4e38ec4db3ede04
Sihamso <sihame.touati@example.com>
2023-04-20 15:42:58 -0700

    ur mum

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,14 +10,8 @@ void print_all(const char * const format, ...)
 {
 int i = 0;
 char *str, *sep = "";
-
-
 va_list list;
-
-
 va_start(list, format);
-
-
 if (format)
 {
 while (format[i])
@@ -47,9 +41,6 @@ sep = ", ";
 i++;
 }
 }
-
-
 printf("\n");
 va_end(list);
 }
-
